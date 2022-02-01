42b2a37ebf30a1b3da5d7601936e50752492a5df
Artem Kulyhin <akulyhin@gmail.com>
2022-02-01 14:50:34 +0200

    Last updates

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 const ws = new require('ws');
 
 const wsServer = new ws.Server({port: 5000});
+
+const users = [];
+
+wsServer.on('connection', (newUser) => {
+    users.push(newUser);
+
+    newUser.on('message', (data) => {
+        const message = JSON.parse(data);
+        const sendMessage = JSON.stringify(message);
+
+        users.forEach(user => {
+            if (user !==newUser) {
+                user.send(sendMessage)
+            }
+        })
+    })
+});
